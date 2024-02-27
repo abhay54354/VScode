4776d994be5a4cf28bbca4a927fe76f006c7ba67
abhay54354 <salunkheabhay437@gmail.com>
2024-02-27 21:57:36 +0530

    mark

---
+++ python/mark.py
@@ -0,0 +1,10 @@
+a=int(input("enter your python mark:"))
+b=int(input("enter your java mark:"))
+c=int(input("enter your C.N. mark:"))
+d=int(input("enter your D.A.A. mark:"))
+e=int(input("enter your C.W. mark:"))
+total = a+b+c+d+e
+
+percentage =total/5*100
+print("your percentage ",percentage )
+
